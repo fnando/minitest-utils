e5e8a3998f72b877dd147d6ef89aa0911c9263e9
Nando Vieira <fnando.vieira@gmail.com>
2016-03-13 10:06:49 -0300

    Also define url helpers in controller tests.

--- lib/minitest/utils/rails.rb
+++ lib/minitest/utils/rails.rb
@@ -1,3 +1,20 @@
+module Minitest
+  module Utils
+    module UrlHelpers
+      include Rails.application.routes.url_helpers
+
+      def default_url_options
+        config = Rails.configuration
+
+        Rails.application.routes.default_url_options ||
+          config.action_controller.default_url_options ||
+          config.action_mailer.default_url_options ||
+          {}
+      end
+    end
+  end
+end
+
 module ActiveSupport
   class TestCase
     extend Minitest::Spec::DSL if defined?(Minitest::Spec::DSL)
@@ -19,20 +36,12 @@ end
 
 module ActionController
   class TestCase
-    include Rails.application.routes.url_helpers
-
-    def default_url_options
-      Rails.configuration.action_mailer.default_url_options
-    end
+    include Minitest::Utils::UrlHelpers
   end
 end
 
 module ActionMailer
   class TestCase
-    include Rails.application.routes.url_helpers
-
-    def default_url_options
-      Rails.configuration.action_mailer.default_url_options
-    end
+    include Minitest::Utils::UrlHelpers
   end
 end
--- lib/minitest/utils/rails/locale.rb
+++ lib/minitest/utils/rails/locale.rb
@@ -1,6 +1,29 @@
-class ActionDispatch::IntegrationTest
-  setup do
-    Rails.application.routes.default_url_options[:locale] = I18n.locale
-    Rails.configuration.action_mailer.default_url_options = Rails.application.routes.default_url_options if defined?(ActionMailer)
+module ActionDispatch
+  class IntegrationTest
+    include Minitest::Utils::UrlHelpers
+
+    setup do
+      Rails.application.routes.default_url_options[:locale] = I18n.locale
+    end
+  end
+end
+
+module ActionController
+  class TestCase
+    include Minitest::Utils::UrlHelpers
+
+    setup do
+      Rails.application.routes.default_url_options[:locale] = I18n.locale
+    end
+  end
+end
+
+module ActionMailer
+  class TestCase
+    include Minitest::Utils::UrlHelpers
+
+    setup do
+      Rails.application.routes.default_url_options[:locale] = I18n.locale
+    end
   end
 end
