38aaea893511db171601abe1c3e83215a8b67e2c
Nando Vieira <fnando.vieira@gmail.com>
2016-04-13 13:02:01 -0300

    Make the setup and teardown strategy for locale configurable.

--- lib/minitest/utils/rails/locale.rb
+++ lib/minitest/utils/rails/locale.rb
@@ -1,29 +1,49 @@
+module Minitest
+  module Utils
+    module Locale
+      class << self
+        attr_accessor :setup
+        attr_accessor :teardown
+      end
+
+      self.setup = proc do
+        Rails.application.routes.default_url_options[:locale] = I18n.locale
+      end
+
+      self.teardown = proc do
+        Rails.application.routes.default_url_options.delete(:locale)
+      end
+
+      def self.included(base)
+        base.setup do
+          instance_eval(&Minitest::Utils::Locale.setup)
+        end
+
+        base.teardown do
+          instance_eval(&Minitest::Utils::Locale.teardown)
+        end
+      end
+    end
+  end
+end
+
 module ActionDispatch
   class IntegrationTest
     include Minitest::Utils::UrlHelpers
-
-    setup do
-      Rails.application.routes.default_url_options[:locale] = I18n.locale
-    end
+    include Minitest::Utils::Locale
   end
 end
 
 module ActionController
   class TestCase
     include Minitest::Utils::UrlHelpers
-
-    setup do
-      Rails.application.routes.default_url_options[:locale] = I18n.locale
-    end
+    include Minitest::Utils::Locale
   end
 end
 
 module ActionMailer
   class TestCase
     include Minitest::Utils::UrlHelpers
-
-    setup do
-      Rails.application.routes.default_url_options[:locale] = I18n.locale
-    end
+    include Minitest::Utils::Locale
   end
 end
